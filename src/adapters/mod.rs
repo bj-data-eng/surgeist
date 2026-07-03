970391aa2476a030628aa8300b7f2c68257a5cbf
bj-data-eng <larks-embassy.5j@icloud.com>
2026-07-03 13:27:32 -0400

    Move style layout adapter to root

--- src/adapters/mod.rs
+++ src/adapters/mod.rs
@@ -4,8 +4,16 @@
 //! composes public Surgeist crate contracts.
 
 mod error;
+mod style_layout;
+
+#[cfg(test)]
+mod style_layout_tests;
 
 #[cfg(test)]
 mod tests;
 
 pub use error::{AdapterBoundary, AdapterError, AdapterErrorKind, AdapterResult};
+pub use style_layout::{
+    LayoutLoweringOutput, LayoutLoweringSession, lower_style_to_layout,
+    lower_style_to_layout_with_store,
+};
